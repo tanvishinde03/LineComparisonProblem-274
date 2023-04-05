a6ea3f2bbff771b82863375ac6c975424c178afe
Tanvishinde03 <shindetanvi0203@gmail.com>
2023-04-05 17:27:50 +0530

    [Tanvi] Add : UC1 - Calculating Length of Line

---
+++ LineComparisonProblem-274/CalculateLength.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem_274
+{
+    public class CalculateLength
+    {
+        int lengthX, _lengthY, lenGthsX, lengthsY;
+        public CalculateLength(int lengthX, int lengthY, int lengthsX, int lengthsY)
+        {
+            this.lengthX = lengthX;
+            _lengthY = lengthY;
+            lenGthsX = lengthsX;
+            this.lengthsY = lengthsY;
+        }
+        public double Calculate()
+        {
+            double length = Math.Sqrt(Math.Pow((lenGthsX - lengthX), 2) + Math.Pow((lengthsY - _lengthY), 2));
+            Console.WriteLine("Lenght of the line is : " + length);
+            return length;
+        }
+    }
+}
+
+
---
+++ LineComparisonProblem-274/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem_274
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculateLength length = new CalculateLength(4, 8, 3, 5);
+            length.Calculate();
+            Console.ReadLine();
+        }
+    }
+}
